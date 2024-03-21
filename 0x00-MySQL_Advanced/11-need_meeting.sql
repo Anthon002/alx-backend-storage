3b3df26737925297c13decb41994b0fb35a75fa8
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-03-21 23:17:49 +0100

    view

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,4 @@
--- creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month
+-- create need_meeting view
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS 
 SELECT name FROM students 
@@ -6,4 +6,4 @@ WHERE
     score < 80 AND 
     (last_meeting IS NULL 
         OR 
-    last_meeting < ADDDATE(CURDATE(), interval -1 MONTH));
+    last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
