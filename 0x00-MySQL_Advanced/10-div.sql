fd7b6d2c56717875f6c888bf2f2534c42e63649a
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-03-21 23:15:37 +0100

    div

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,4 +1,4 @@
--- createscreates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0
+-- this create a function
 DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
