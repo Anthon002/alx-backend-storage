72f4bcf1941cea1ac3ed9e8cbf7fbdc53ac3ebc1
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-03-21 23:00:42 +0100

    glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
---  lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+-- Retrieve bands specializing in Glam rock and sort them by their longevity
+SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS longevity
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+ORDER BY longevity DESC;
